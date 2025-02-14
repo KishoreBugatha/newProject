8c0efe02c0a9a7772ab6f085ac3feeaeeaa06693
Kishore Reddy <kishore_reddy@localhost.localdomain>
2025-02-15 00:54:27 +0530

    This is a new change in feature branch

--- Prime.java
+++ Prime.java
@@ -18,8 +18,10 @@ return true;
 public static void main(String[] args)
 {
 System.out.println("Enter a number : ");
+
 Scanner sc=new Scanner(System.in);
 int n=sc.nextInt();
 System.out.println(prime(n));
+System.out.println("This is a prime number program");
 }
 }
