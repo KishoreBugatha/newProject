4caa47708482802404ea76a0dc39ec0a8ae80035
Kishore Reddy <kishore_reddy@localhost.localdomain>
2025-02-15 00:05:03 +0530

    This is commit 1

---
+++ Prime.java
@@ -0,0 +1,25 @@
+import Math;
+import java.util.*;
+class Prime{
+int num;
+bool prime(int n)
+{
+if(n%2==0)
+{
+return false;
+}
+else{
+for(int i=3;i<Math.sqrt(n,2);i++)
+{
+if(n%i==0) return false;
+}
+return true;
+}
+public static void main(String[] args)
+{
+System.out.println("Enter a number : ");
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+System.out.println(prime(n));
+}
+}
